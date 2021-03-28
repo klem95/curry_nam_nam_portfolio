abe48228738bf08b590d8b1e4c23e64e54b98fdb
klem95 <enxbox@hotmail.com>
2021-03-28 11:50:06 +0200

    siteUrl added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "My Gatsby Site",
+    siteUrl: "https://needstobetheproductionurl.com",
   },
   plugins: [
     "gatsby-plugin-styled-components",
