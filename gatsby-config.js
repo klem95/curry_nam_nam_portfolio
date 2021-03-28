ac65152c7c5ce60e3e6777e4d08aa389f3fb1f51
klem95 <enxbox@hotmail.com>
2021-03-28 14:51:47 +0200

    bug fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const { resolve } = require("./public/render-page");
+//const { resolve } = require("./public/render-page");
 
 const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 require("dotenv").config({path: `.env.${activeEnv}`,})
